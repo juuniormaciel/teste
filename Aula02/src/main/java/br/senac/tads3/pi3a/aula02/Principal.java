975b1f32dcc3f5097fa3850ee3b75e9055e4bb0e
antonio.cmajunior <antonio.cmajunior@CAS555503W0705.senacsp.edu.br>
2017-08-18 21:41:30 -0300

    versão inicial !

---
+++ Aula02/src/main/java/br/senac/tads3/pi3a/aula02/Principal.java
@@ -0,0 +1,14 @@
+
+package br.senac.tads3.pi3a.aula02;
+
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World git maven");
+    }
+    
+}
